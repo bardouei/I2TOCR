89b88cbe2650cf3944088bd1e72a5f2bc8a7b3bb
sadegh bardouei <sadegh.bardouei@gmail.com>
2023-06-04 12:04:23 +0330

    create project

---
+++ I2TORC/I2TORCApp.swift
@@ -0,0 +1,17 @@
+//
+//  I2TORCApp.swift
+//  I2TORC
+//
+//  Created by sadegh bardouei on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct I2TORCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
