11c3a10b704ea9d6695a77ebc60ff57c877ec7f7
sadegh bardouei <sadegh.bardouei@gmail.com>
2023-06-13 19:45:04 +0330

    print mamad

--- I2TORC/I2TORCApp.swift
+++ I2TORC/I2TORCApp.swift
@@ -24,6 +24,7 @@ struct I2TORCApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        print("mamad")
         return true
     }
     
